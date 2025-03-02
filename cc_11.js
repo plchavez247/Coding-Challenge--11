41b7f4b9c077d249b7d53154940db0278a1ba2b6
Pablo Chavez <plchavez@usf.edu>
2025-03-01 20:07:52 -0500

    Task 2 - Created Borrower Class

--- cc_11.js
+++ cc_11.js
@@ -1,3 +1,4 @@
+//Task 1: Creating a book class
 class Book{
     constructor (title, author, isbn, copies){
         this.title = title;
@@ -22,4 +23,27 @@ console.log (book1.getDetails());
 book1.updateCopies(-1);
 console.log(book1.getDetails());
 
+//Task 2: Creating a borrower class
+
+class Borrower{
+    constructor(name, borrowerId, borrowedBooks = []){
+        this.name = name;
+        this.borrowerId = borrowerId;
+        this.borrowedBooks = borrowedBooks;
+    };
+
+    borrowBook(book){
+        this.borrowedBooks.push(book);
+    }
+
+    returnBook(book){
+        this.borrowedBooks = this.borrowedBooks.filter(a => a !== book);//creates new array without specified book
+    }
+};
+
+const borrower1 = new Borrower ("Alice Johnson", 201);
+borrower1.borrowBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
+borrower1.returnBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
 
